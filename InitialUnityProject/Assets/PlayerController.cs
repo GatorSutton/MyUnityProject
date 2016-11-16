5076a89363b3dada7d1bc506969ba6faa49cdfe4
GatorSutton <garrettsuttonee@gmail.com>
2016-11-15 23:27:43 -0500

    Player Movement and Jumping
    Added Gameobjects for the floor and a controller on the player to move
    left, right, and jump.

---
+++ InitialUnityProject/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public float jumpHeight = 200f;
+    bool facingRight = true;
+
+    public Transform groundPoint;
+    public float radius;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector2 moveDir = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb2d.velocity.y);
+        rb2d.velocity = moveDir;
+
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, radius, groundMask);
+
+        if(Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            facingRight = true;
+        }else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            facingRight = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb2d.AddForce(new Vector2(0, jumpHeight));
+        }
+        
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundPoint.position, radius);
+    }
+}
